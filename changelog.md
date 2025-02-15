887112e427fcaf0ed748fdce04d8f2fd8d7dfcfc
Kaf <coffeechopdev@gmail.com>
2025-02-15 16:18:23 -0300

    feat: added the new torches to the functional blocks creative tab feat(1.21.1): 1.21.1 port

--- changelog.md
+++ changelog.md
@@ -2,6 +2,11 @@
 
 See the full changelog at https://github.com/iamkaf/torch-toss/commits/main/
 
+## 1.2.0
+
+- feat: added the new torches to the functional blocks creative tab
+- feat(1.21.1): 1.21.1 port
+
 ## 1.1.0
 
 - feat: added throwable soul torches
--- fabric/src/main/java/com/iamkaf/torchtoss/fabric/TorchTossFabric.java
+++ fabric/src/main/java/com/iamkaf/torchtoss/fabric/TorchTossFabric.java
@@ -1,10 +1,23 @@
 package com.iamkaf.torchtoss.fabric;
 
-import net.fabricmc.api.ModInitializer;
-
+import com.iamkaf.torchtoss.ModItems;
 import com.iamkaf.torchtoss.TorchToss;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Items;
 
 public final class TorchTossFabric implements ModInitializer {
+    public static void registerItemsToCreativeTab(FabricItemGroupEntries event) {
+        event.addAfter(
+                Items.REDSTONE_TORCH.getDefaultInstance(),
+                ModItems.THROWABLE_REDSTONE_TORCH.get().getDefaultInstance()
+        );
+        event.addAfter(Items.REDSTONE_TORCH.getDefaultInstance(), ModItems.THROWABLE_SOUL_TORCH.get().getDefaultInstance());
+        event.addAfter(Items.REDSTONE_TORCH.getDefaultInstance(), ModItems.THROWABLE_TORCH.get().getDefaultInstance());
+    }
+
     @Override
     public void onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -13,5 +26,8 @@ public final class TorchTossFabric implements ModInitializer {
 
         // Run our common setup.
         TorchToss.init();
+
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS)
+                .register(TorchTossFabric::registerItemsToCreativeTab);
     }
 }
