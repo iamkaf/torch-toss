3cde884ecb4f39fc85e1683c10499f20b824378d
Kaf <coffeechopdev@gmail.com>
2026-07-09 01:05:15 -0300

    Add LambDynamicLights support

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -5,3 +5,19 @@ plugins {
 extensions.configure<com.iamkaf.multiloader.fabric.MultiloaderFabricExtension>("multiloaderFabric") {
     commonDatagen.set(true)
 }
+
+dependencies {
+    if (name == "26.2") {
+        // LambDynamicLights
+        runtimeOnly("maven.modrinth:yBW8D80W:4.12.2+26.2")
+    }
+}
+
+subprojects {
+    dependencies {
+        if (name == "26.2") {
+            // LambDynamicLights
+            runtimeOnly("maven.modrinth:yBW8D80W:4.12.2+26.2")
+        }
+    }
+}
--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -1,3 +1,19 @@
 plugins {
     id("com.iamkaf.multiloader.neoforge")
 }
+
+dependencies {
+    if (name == "26.2") {
+        // LambDynamicLights
+        runtimeOnly("maven.modrinth:yBW8D80W:4.12.2+26.2")
+    }
+}
+
+subprojects {
+    dependencies {
+        if (name == "26.2") {
+            // LambDynamicLights
+            runtimeOnly("maven.modrinth:yBW8D80W:4.12.2+26.2")
+        }
+    }
+}
